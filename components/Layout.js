b5a60d7a4b0190cb454db818ed498e718128ec43
Javier Ocampo <ferroxido@gmail.com>
2019-06-21 18:45:31 -0300

    Add some antd components

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
 
 const { Header, Footer, Content } = Layout;
 
@@ -12,7 +12,21 @@ export default class LayoutComponent extends React.Component {
         <Head>
           <title>{title}</title>
         </Head>
-        <Header>Header</Header>
+        <Header>
+          <div className="logo" />
+          <Menu
+            mode="horizontal"
+            theme="dark"
+            style={{ lineHeight: "64px" }}
+            defaultSelectedKeys={["2"]}
+          >
+            <Menu.Item theme="dark" key="1">
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
         <Content>{children}</Content>
         <Footer>Footer</Footer>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
+import { Carousel } from "antd";
 import LayoutComponent from "../components/Layout";
 
 export default class extends React.Component {
   render() {
     return (
       <LayoutComponent title="Dev&Coffee">
-        <h1>Dev&Coffee</h1>
+        <Carousel autoplay>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+        </Carousel>
+        <style>{`
+          .ant-carousel .slick-slide {
+            text-align: center;
+            height: 300px;
+            line-height: 300px;
+            background: #364d79;
+            overflow: hidden;
+          }
+
+          .ant-carousel .slick-slide h3 {
+            color: #fff;
+          }
+        `}</style>
       </LayoutComponent>
     );
   }
